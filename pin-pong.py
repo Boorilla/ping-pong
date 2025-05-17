e935b5a5e2bf01a9fe442d6f77118213fed2c794
Boorilla <supogrp@gmail.com>
2025-05-17 18:15:51 +0400

    Закончил работу над основной частью игры

--- pin-pong.py
+++ pin-pong.py
@@ -39,8 +39,14 @@ clock = time.Clock()
 FPS = 60
 
 #создание мяча и ракетки
+ball = GameSprite('tenis_ball.png', 200, 200, 50, 50,4)
 racket1 = Player('racket.png', 10, 200, 50, 150, 4)
 racket2 = Player('racket.png', 540, 200, 50, 150, 4)
+
+font.init()
+font1 = font.Font(None,35)
+lose1 = font1.render('Player 1 LOSE!',True, (180,0,0))
+lose2 = font1.render('Player 2 LOSE!',True, (180,0,0))
 
 speed_x = 3
 speed_y = 3
@@ -55,5 +61,21 @@ while run:
         racket2.update_r()
         racket1.reset()
         racket2.reset()
+        ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+        # если мяч достигает границ экрана (сверху и снизу) меняем направление его движ if ball.rect.y > win_height-50 or ball.rect.y < 0:
+    if ball.rect.y > win_height-50 or ball.rect.y <0    :
+        speed_y *= -1
+        # если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+        # если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
+    if ball.rect.x > win_width:
+        finish = True
+        window.blit(lose2, (200, 200))
     display.update()
     clock.tick(FPS)
